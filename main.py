c04c5f89bf71b4ab457829882886fe812a965591
ALARCON503 <alarcont.julio@gmail.com>
2025-07-21 21:25:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 from rembg import remove
 from io import BytesIO
+import uvicorn
 
-app = FastAPI(title="API Quitar Fondo", description="Quita fondo de imágenes con IA usando rembg", version="1.0")
+app = FastAPI(
+    title="API Quitar Fondo",
+    description="Quita fondo de imágenes con IA usando rembg",
+    version="1.0"
+)
 
 @app.get("/")
 def read_root():
@@ -11,15 +17,12 @@ def read_root():
 
 @app.post("/remove-bg")
 async def remove_background(file: UploadFile = File(...)):
-    # Leer imagen subida
     image_bytes = await file.read()
-
-    # Procesar imagen con rembg
     result = remove(image_bytes)
-
-    # Convertir resultado a BytesIO para poder enviarlo como archivo
     output = BytesIO(result)
     output.seek(0)
-
-    # Devolver imagen sin fondo
     return StreamingResponse(output, media_type="image/png")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
